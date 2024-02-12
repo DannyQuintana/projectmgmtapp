8e5bbea99849c25b1d83392ed2d5321f58706250
Daniel Quintana <daniel.j.quintana3391@gmail.com>
2024-02-12 12:53:04 -0800

    Initial Commit

---
+++ src/main/java/wgustudentproject/d424/D424Application.java
@@ -0,0 +1,13 @@
+package wgustudentproject.d424;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D424Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D424Application.class, args);
+	}
+
+}
---
+++ src/test/java/wgustudentproject/d424/D424ApplicationTests.java
@@ -0,0 +1,13 @@
+package wgustudentproject.d424;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D424ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
